83793a46df377256c20064901d66367cd52cd5b8
Filipa Barros <up201305683@fe.up.pt>
2019-05-16 14:35:38 +0200

    Update part1.py

--- part1.py
+++ part1.py
@@ -10,79 +10,86 @@ import matplotlib.pyplot as plt
 import time
 
 
-
-def convert_to_int(date):
-    total = (date.year* 31556926* 1000000) + (date.month *1000000 *2629743.83) + (date.day * 1000000 * 86400) + (date.hour * 3600 * 1000000) + (date.minute * 1000000 * 60) + (date.second*1000000) + date.microsecond
-    #print(int(total))
-    #print(date.microsecond)
-    #print(total)
-   #seconds = seconds 
-    return total
-
+#creating the socket -> Task 1
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
 address = "129.187.223.200"
 port = 2000
 
-#testing the socket 
-packet = "TEST".ljust(1000);
-packet_size = len(packet);
+#testing the socket -> Task 2
+packet = "TEST".ljust(400);
+sock.sendto(packet.encode(),(address,port));
+data = sock.recv(400);
+print('message received:', data);
+
+#sending two back to back packets and recording the time -> task 3.a, 3.b and 3.c
+packet = "a".ljust(1000);
+sock.sendto(packet.encode(),(address,port));
+sock.sendto(packet.encode(),(address,port));
+data = sock.recv(1000);
+ts1 = int(time.time_ns());
+data = sock.recv(1000);
+ts2 = int(time.time_ns());
+delay= abs(ts2 - ts1);
+print('The delay for the two small back to back packets is ',  delay, 'nanoseconds');
+if (delay == 0):
+    bottleneck = 0;
+else:
+    bottleneck = len(packet)/delay;
+print('The bottleneck is ',  bottleneck);
 
 
-delays = [];    
-delay = 0;
-bottlenecks = [];
-bottleneck = 0;
-receiving_time = [];
-bottlenecks_means = [];
-bottleneck_sum = 0;
+
+#task 3.d abd 3.e -> repeating the measure for 200 times for each packet size
 
 
-for j in range (1000,10000,100):
-    packet = "TEST".ljust(j);
+r1 = 200;
+r2 = 1500;
+pace = int((r2 - r1)/5)
+
+bottlenecks = [[],[],[],[],[],[]];
+
+n = 0;
+for j in range (r1,r2+1,pace):
+    packet = "a".ljust(j);
+    packet_size = len(packet);
     bottleneck_sum = 0;
-    for i in range (200):
+    i = 0;
+    while i <= 200:
+        print(i);
         sock.sendto(packet.encode(),(address,port));
-        data = sock.recv(10000);
-        ts = int(time.time_ns())
-        receiving_time.append(ts);
-        if (i > 0):
-            delay = abs(receiving_time[i] - receiving_time[i-1])
-            delays.append(delay);
-            if (delay == 0):
-                bottleneck = 0;
-            else: 
-                bottleneck = packet_size/delay;   #bits/nanoseconds
-            bottlenecks.append(bottleneck)
-            bottleneck_sum += bottleneck
-    bottlenecks_means.append(bottleneck_sum/199);
-
-x = list(range(1000,10000,100));
-y = bottlenecks_means;
-plt.plot(x, y, 'b-', label='data')
-plt.plot(x, y, 'g-', linewidth=2, label='filtered data')
-plt.xlabel('N')
-plt.grid()
-plt.legend()
-
-print('lala')  
- 
-
-
-#to dos      -> ajustar a fórmula para a diferença de tempo -> rever
-#            -> preencher a string com white space
-#            -> fazer uma função para ir mudando o tamanho dos pacotes 
-#            -> fazer um ciclo de 200 para cada um dos tamanhos de pacote que calcula o delay entre dois pacotes backtoback
-#            -> descobrir como é que se faz para mandar valores quando se corre o ficheiro
-#            -> fazer a fórmula para o bottleneck link 
-#            -> fazer os gráficos com os bottlenecks vs o tamanho dos pacotes
- 
-
-#Doubts: 
-    #Mean bottleneck?
-    #packets size?
+        sock.sendto(packet.encode(),(address,port));
+        data = sock.recv(j);
+        ts1 = int(time.time_ns());
+        data = sock.recv(j);
+        ts2 = int(time.time_ns());
+        delay= ts2 - ts1;
+        if (delay == 0):
+            i = i - 1;
+        else: 
+            bottleneck = packet_size/delay;
+            bottlenecks[n].append(bottleneck)
+        i = i + 1;
+    n = n + 1;
+    print(n);
+    print(j);
     
-#code for time adjustmnent: t = datetime.datetime.now()
-#                           print(time.mktime(t.timetupe()))
+
+#plot the bottlenecks for each packet size
+x = list(range(0,201))
+fig = plt.figure(figsize=(10,10))
+for i in range(len(bottlenecks)):
+    print (len(bottlenecks[i]));
+    p = r1 + i*pace;    
+    l = 'bottlenecks from size packet ' + str(p);
+    y = bottlenecks[i];
+    ax = fig.add_subplot(6, 1, i + 1)
+    ax.plot(x,y, 'o', linewidth=2, markersize=2, label=l);
+    ax.set_ylabel('Bottleneck')
+    ax.set_xlabel('N')
+    ax.legend()
+    print (i);
+
+print(pace)
+
 
 
